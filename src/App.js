5b679bebaaf90067e46066dccf4b8fab7cd3b7da
David Lazaro <dlazaro7@gmail.com>
2022-11-01 22:05:35 +0100

    iteration 3 Full Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import storedContacts from "./contacts.json";
+import { useState } from "react";
+import storedContactsArr from "./contacts.json";
 import "./App.css";
 
-let contactsArr = storedContacts;
-const firstFiveContacts = contactsArr.slice(0, 5);
-
 function App() {
+  const firstFiveContacts = storedContactsArr.slice(0, 5);
+
+  const [famousList, setFamousList] = useState(firstFiveContacts);
+
+  const randomPerson = () => {
+    let randomArr = Math.random() * storedContactsArr.length;
+    let randomFamous = Math.floor(randomArr);
+    let newPerson = storedContactsArr[randomFamous];
+
+    const copyFamousList = [...famousList];
+
+    
+    copyFamousList.includes(newPerson) === false && copyFamousList.push(newPerson)
+
+
+
+    setFamousList(copyFamousList)
+    // if (copyFamousList.includes(newPerson) === false) {
+    //   copyFamousList.push(newPerson);
+    //  setFamousList(copyFamousList);
+    //  }
+
+  };
+
   return (
     <div className="App">
+      <h1>IronContacts</h1>
+      <button onClick={randomPerson}>Add Random Contact</button>
       <table style={{ margin: "20px 0px 0px 200px", width: "40%" }}>
         <thead>
           <tr>
@@ -19,7 +43,7 @@ function App() {
         </thead>
 
         <tbody>
-          {firstFiveContacts.map((eachPerson) => {
+          {famousList.map((eachPerson) => {
             return (
               <tr key={eachPerson.id}>
                 <td>
@@ -27,7 +51,9 @@ function App() {
                 </td>
                 <td>{eachPerson.name}</td>
                 <td>{eachPerson.popularity}</td>
-                <td>{eachPerson.wonOscar === true ? <p>trophy</p>: <p></p>}</td>
+                <td>
+                  {eachPerson.wonOscar === true ? <p>🏆</p> : <p></p>}
+                </td>
                 <td>{eachPerson.wonEmmy}</td>
               </tr>
             );
